292feffc0dd8e9beaa46451d1328ce7537d394bc
TRI - Jay Reyes <j.reyes@tricentis.com>
2019-02-26 08:39:31 -0600

    add tests for other get inputs

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 from unittest import mock
 
 from get_inputs import (
@@ -7,23 +8,53 @@ from get_inputs import (
     get_title,
     get_time, 
     get_notes,
-    get_keyword, 
+    get_keyword,
+    get_parsed_date 
     )
 
 
 class WorkLogTest(unittest.TestCase):
     
-    @mock.patch('builtins.input')
-    def test_get_user(self, mocked_input):
-        mocked_input.return_value = "jay"
-        self.assertEqual(get_user(), "jay")
 
-    @mock.patch('builtins.input')
+    @mock.patch('builtins.input', side_effect=['jay'])
+    def test_get_user_jay(self, mocked_input):
+        result = get_user()
+        self.assertEqual(result, 'jay')
+
+    @mock.patch('builtins.input', side_effect=['', '\n'])
+    def test_get_user_jay(self, mocked_input):
+        result = get_user()
+        self.assertEqual(result, 'jay')
+
+    @mock.patch('builtins.input', side_effect=['12/12/2004'])
     def test_get_date(self, mocked_input):
-        mocked_input.side_effect = "11/11/20102"
-        self.assertEqual(get_user(), "11/11/20102")
+        result = get_date()
+        self.assertEqual(result, '12/12/2004')
 
-       
+    @mock.patch('builtins.input', side_effect=['Sim Title'])
+    def test_get_title(self, mocked_input):
+        result = get_title()
+        self.assertEqual(result, 'Sim Title')
+    
+    @mock.patch('builtins.input', side_effect=['40'])
+    def test_get_time(self, mocked_input):
+        result = get_time()
+        self.assertEqual(result, 40)
+    
+    @mock.patch('builtins.input', side_effect=['Sim Notes'])
+    def test_get_notes(self, mocked_input):
+        result = get_notes()
+        self.assertEqual(result, 'Sim Notes')
+    
+    @mock.patch('builtins.input', side_effect=['Sim Keyword'])
+    def test_get_keyword(self, mocked_input):
+        result = get_keyword()
+        self.assertEqual(result, 'Sim Keyword')
+
+    @mock.patch('builtins.input', side_effect=['11/11/2009'])
+    def test_get_keyword(self, mocked_input):
+        result = get_parsed_date("first")
+        self.assertEqual(result, datetime(2009, 11, 11, 0, 0))
 
 
 if __name__ == "__main__":
